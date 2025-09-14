ba1af8af215091a58c4a7105ca194ddc63c2e5a5
JordanDiza <dizaj8@gmail.com>
2025-09-13 20:09:10 -0400

    49

--- TuneCreator.java
+++ TuneCreator.java
@@ -25,9 +25,8 @@ public class TuneCreator
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public int sampleMethod(int y)
+    public void CreateAndPlay()
     {
-        // put your code here
-        return  y;
+        
     }
 }
